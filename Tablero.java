5c5a0cb85cfaeba7e8fc8589a0d775e173e66cdd
alvariikoo <afuentejimenez@gmail.com>
2024-12-05 02:00:48 +0100

    Add files via upload

--- Tablero.java
+++ Tablero.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Tablero {
 
@@ -12,20 +14,50 @@ public class Tablero {
         for(int i= 0;i<tablero.length;i++){
             for(int j=0;j<tablero[i].length;j++){
                 
-                tablero[i][j] = new Casilla(i,j,null,null);
-                
+                tablero[i][j] = new Casilla(i,j,new ArrayList<>(),null);
+            }
+            
+            for(i=0;i<5;i++){
+                int x = (int) (Math.random()*10);
+                int y = (int) (Math.random()*10);
+           
+            tablero[x][y].setEquipo(generarProvision());
+            
+            }
+            
+            for(i = 0;i<tablero.length;i++){
+                for(int p=0;p<tablero[i].length;p++){
+                    
+                    if(tablero[i][p].getEquipo() == null){
+                        tablero[i][p].setEquipo(generarArma());
+                    }
+                        
+                }
             }
         }
+        
+        tablero[0][0].setEntidad(new ArrayList<>(Arrays.asList(generarSuperviviente())));
+        
+        for(int q =0;q<5;q++){
+                int x = (int) (Math.random()*10);
+                int y = (int) (Math.random()*10);
+           
+             
+                tablero[x][y].setEntidad(new ArrayList<>(Arrays.asList(generarZombiAleatorio())));
+
+            
+            }
+
     }
-    
     
     public Casilla getCasilla(int x, int y){
-      if (x >= 0 && x < tablero.length && y >= 0 && y < tablero[0].length) {
-          return tablero[x][y];
-      }else{
-          return null;
-      }
+        if (x >= 0 && x < tablero.length && y >= 0 && y < tablero[0].length) {
+            return tablero[x][y];
+        }else {
+            return null;
+        }
     }
+
     
     public void mostrarTablero() {
         
@@ -40,8 +72,13 @@ public class Tablero {
     }
     
     private Equipo generarArma(){
-       
         
+        int random = (int) (Math.random()*2);
+        
+        if(random ==0){
+            return null;
+        }
+        if(random ==1){
            int random1 = (int) (Math.random()*2);
            
            if(random1 == 0){
@@ -56,7 +93,7 @@ public class Tablero {
                armas[6] = new Arma("Glock 17(R6)", 1, 1, 3, 1);
                armas[7] = new Arma("1911(Resident Evil)", 1, 1, 3, 1);
                armas[8] = new Arma("AWP (CS:GO)",3, 2, 3, 4);
-               armas[9] = new Arma("Apoticon (COD Zombis)",3, 2, 1, 5);
+               armas[9] = new Arma("Apoticon (COD Zombis",3, 2, 1, 5);
                
                int random2 = (int) (Math.random() * 100) + 1;
                
@@ -100,19 +137,94 @@ public class Tablero {
                }
                
            }
+           
+       
+        }
         return null;
     }
     
-    private void generarProvisiones(){
+    
+    private Equipo generarProvision(){
         
-        Provision[] provi = new Provision[0];
+            Provision[] provi = new Provision[6];
+               
+            provi[0] = new Provision("Cerveza",23,"12/12/2028");
+            provi[1] = new Provision("Manzana De Oro",33,"2/1/2018");
+            provi[2] = new Provision("Bebida Energética",3,"31/8/2020");
+            provi[3] = new Provision("¿Setas?",111,"1/2/2011");
+            provi[4] = new Provision("Fabada",2300,"2/7/2078");
+            provi[5] = new Provision("Sopita",500,"5/2/2048");
+            
+           for(int i=0;i<5;i++){
+               return provi[i];
+           }
         
-               provi[0] = new Provision("Sable Laser(Star wars)", 1, "1");
-               provi[1] = new Provision("Motosierra(DOOM)", 2, "1");
-               provi[2] = new Provision("Recortada(DOOM)", 2, "1");
-               provi[3] = new Provision("M9 (Metal Gear Solid",1, "1");
-               provi[4] = new Provision("P90(Fortnite)", 1, "1");
-               provi[5] = new Provision("AK-47(Battlefield 4)",2, "1");
+           return null;
+   }
+    
+    
+    private Entidad[] generarSuperviviente() {
+        Entidad[] supers = new Entidad[4];
+
+            Inventario inventTakeo = new Inventario();
+            Arma[] armasTakeo = new Arma[2];
+            supers[0] = new Superviviente("Takeo", "Vivo", 3, inventTakeo, armasTakeo, 0, 0);
+
+           Inventario inventDempsey = new Inventario();
+            Arma[] armasDempsey = new Arma[2];
+            supers[1] = new Superviviente("Dempsey", "Vivo", 3, inventDempsey, armasDempsey, 0, 0);
+
+            Inventario inventNikolai = new Inventario();
+            Arma[] armasNikolai = new Arma[2];
+            supers[2] = new Superviviente("Nikolai", "Vivo", 3, inventNikolai, armasNikolai, 0, 0);
+
+            Inventario inventRichtofen = new Inventario();
+             Arma[] armasRichtofen = new Arma[2];
+            supers[3] = new Superviviente("Richtofen", "Vivo", 3, inventRichtofen, armasRichtofen, 0, 0);
+
+    return supers;
+}
+    
+    
+    public Entidad generarZombiAleatorio() {
+    int rand = (int) (Math.random() * 100);
+    Zombi zombi;
+
+    if (rand < 60) {
+        int rand1 = (int) (Math.random() * 3);
+        if (rand1 == 0) {
+            zombi = new Zombi(" ", 1, 1, "Caminante", "Normal");
+        } else if (rand1 == 1) {
+            zombi = new Zombi(" ", 1, 1, "Caminante", "Berserker");
+        } else {
+            zombi = new Zombi(" ", 1, 1, "Caminante", "Normal");
+        }
+    } else if (rand < 90) {
+        int rand2 = (int) (Math.random() * 3);
+        if (rand2 == 0) {
+            zombi = new Zombi(" ", 1, 2, "Corredor", "Normal");
+        } else if (rand2 == 1) {
+            zombi = new Zombi(" ", 1, 2, "Corredor", "Berserker");
+        } else {
+            zombi = new Zombi(" ", 1, 2, "Corredor", "Tóxico");
+        }
+    } else {
+        int rand3 = (int) (Math.random() * 3);
+        if (rand3 == 0) {
+            zombi = new Zombi(" ", 3, 1, "Abominacion", "Normal");
+        } else if (rand3 == 1) {
+            zombi = new Zombi(" ", 3, 1, "Abominacion", "Berserker");
+        } else {
+            zombi = new Zombi(" ", 3, 1, "Abominacion", "Tóxico");
+        }
     }
+
+    return zombi;
+}
+
+    
+    
+    
+    
     
 }
